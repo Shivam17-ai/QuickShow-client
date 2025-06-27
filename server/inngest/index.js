7acefd226a0a9b6b23272ebfa628ee8f92429715
Shivam17-ai <sg170105@gamil.com>
2025-06-28 04:23:26 +0530

    model import fix

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,4 +1,5 @@
-import { Inngest } from "inngest";
+import { Inngest, models } from "inngest";
+import Users from "../models/user.js";
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
