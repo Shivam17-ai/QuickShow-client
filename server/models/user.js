efb944e5f96d1af41344c92dd24ab813b48f88ca
Shivam17-ai <sg170105@gamil.com>
2025-06-28 05:48:07 +0530

    user changed

--- server/models/user.js
+++ server/models/user.js
@@ -6,6 +6,6 @@ const userSchema = new mongoose.Schema({
     image: {type: String, required: true}
 })
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model('user', userSchema)
 
 export default User;
